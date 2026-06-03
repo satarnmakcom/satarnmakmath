d912e2da7758b035b70b6433c6b8f1f49c91041e
satarnmakcom <satarnmakcom@gmail.com>
2026-06-03 12:06:10 +0700

    Fix CSP to allow data: URIs for Base64 uploaded images

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,30 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com data:",
+              "img-src 'self' data: blob: https:",
+              "connect-src 'self' https:",
+              "frame-src 'self'",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
   },
 };
 
